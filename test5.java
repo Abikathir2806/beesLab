7ae68d2726620c6391aeabacc9310394c9f6bb8d
Abikathir2806 <70736054+Abikathir2806@users.noreply.github.com>
2020-09-03 23:56:45 -0700

    Add files via upload

---
+++ test5.java
@@ -0,0 +1,55 @@
+public static int getHolidaysDaysBetweenTwoDates(Date startDate, Date endDate) 
+{
+    
+Calendar startCal = Calendar.getInstance();
+    
+startCal.setTime(startDate);        
+
+    
+Calendar endCal = Calendar.getInstance();
+    
+endCal.setTime(endDate);
+
+    
+int holidays = 0;
+
+    
+    
+if (startCal.getTimeInMillis() == endCal.getTimeInMillis()) 
+{
+        
+return 0;
+    
+}
+
+    
+if (startCal.getTimeInMillis() > endCal.getTimeInMillis()) 
+{
+        
+startCal.setTime(endDate);
+        
+endCal.setTime(startDate);
+    
+}
+
+    
+do 
+{
+      
+        
+startCal.add(Calendar.DAY_OF_MONTH, 1);
+       
+ if (startCal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY && startCal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)
+ {
+            
+   ++holidays;
+        
+}
+    
+} 
+while (startCal.getTimeInMillis() < endCal.getTimeInMillis()); 
+
+    
+return holidays;
+
+}
