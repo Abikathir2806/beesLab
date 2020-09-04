f690c3fc9206fc075cc018e303a4e56e2ee4259c
Abikathir2806 <70736054+Abikathir2806@users.noreply.github.com>
2020-09-03 23:49:22 -0700

    Add files via upload

---
+++ test3.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+#include<stdlib.h>  
+ 
+int main()    
+
+{    
+
+int a,b; 
+
+scanf("%d %d",&a,&b); 
+
+printf("Before swap a=%d b=%d",a,b);       
+
+a=a*b;
+
+b=a/b;    
+
+a=a/b;
+
+printf("\nAfter swap without a third variable a=%d b=%d",a,b);       
+
+return 0;  
+
+}
