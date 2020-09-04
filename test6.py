8a81a4e8717fccb7e859e06150f8c981f3b29430
Abikathir2806 <70736054+Abikathir2806@users.noreply.github.com>
2020-09-04 00:10:16 -0700

    Add files via upload

---
+++ test6.py
@@ -0,0 +1,19 @@
+def findAngle(hour, min):
+	
+     h = (hour * 360) // 12 + (min * 360) // (12 * 60)
+	
+     m = (min * 360) // (60) 
+	
+     angle = abs(h - m)
+	
+     if angle > 180:
+		
+       angle = 360 - angle
+         
+     return angle
+        
+     hour = int(input()) 
+	
+     min = int(input()) 
+        
+     print(findAngle(hour, min))
