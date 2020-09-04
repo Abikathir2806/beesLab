9ee50c724a620759ccf6e89878fc561cec36e304
Abikathir2806 <70736054+Abikathir2806@users.noreply.github.com>
2020-09-03 23:51:59 -0700

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,21 @@
+s1=input().strip() 
+
+s2=input().strip()
+
+s1=int(s1)
+
+s2=int(s2)
+
+c=0
+
+for i in range(s1,s2+1):
+    
+      s=str(i) 
+    
+      for j in s:
+        
+           if j=='1':
+            
+              c=c+1 
+
+print(c)
